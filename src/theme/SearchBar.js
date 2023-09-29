770bac4688f5038e5368a35dbeb7a77bbc3d6ac8
Paul Blankley <paul@zenlytic.com>
2023-09-29 09:16:18 -0400

    fix white on white text in light mode in search (code)

--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { MendableSearchBar } from "@mendable/search";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useThemeContext from '@theme/hooks/useThemeContext';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
 
@@ -8,13 +9,14 @@ export default function SearchBarWrapper() {
   const {
     siteConfig: { customFields },
   } = useDocusaurusContext();
+  const { isDarkTheme } = useThemeContext();
   return (
     <div className="mendable-search">
         <BrowserOnly>
             {() => {
             return <MendableSearchBar
                 anon_key={customFields.mendableAnonKey}
-                style={{ accentColor: "#ECFFA1", darkMode: true, ariaLabel: 'Search' }}
+                style={{ accentColor: "#ECFFA1", darkMode: isDarkTheme, ariaLabel: 'Search' }}
                 placeholder="Search..."
                 dialogPlaceholder="How to I create a view?"
                 showSimpleSearch={true}
